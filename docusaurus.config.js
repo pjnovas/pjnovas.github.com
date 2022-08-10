2d0041144395904b310852a989a35fa87dec3da7
Pablo Novas <pjnovas@gmail.com>
2022-08-09 22:19:40 -0300

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,62 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'La bitácora de Pablo',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'pjnovas', // Usually your GitHub org/user name.
+  projectName: 'pjnovas.github', // Usually your repo name.
+
+  i18n: {
+    defaultLocale: 'es-AR',
+    locales: ['es-AR'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: false,
+        blog: {
+          routeBasePath: '/', 
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'La bitácora de Pablo',
+        logo: {
+          alt: 'Blog Logo',
+          src: 'img/logo.svg',
+        },
+        items: [],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
